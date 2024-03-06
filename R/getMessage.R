0a31bf87b451e489f01a36e6d00232196976cfd3
AlexIvanHoward <alexivanhoward@gmail.com>
2024-03-06 12:58:42 +0200

    Formatting and commenting

--- R/getMessage.R
+++ R/getMessage.R
@@ -10,27 +10,36 @@ getMessage <- function(messageId,
                        network = c("mainnet", "previewnet", "testnet")[3],
                        decode = FALSE) {
   
+  # Determine network base URL.
   baseUrl <- getNetworkBaseUrl(network)
+  
+  # Build the query URL.
   xURL <- sprintf("%sapi/v1/topics/messages/%s", baseUrl, messageId)
   
+  # Run the query.
   res <- httr::GET(xURL)
   
-  if (res$status_code != 200) {
-    statCode <- res$status_code
-    res <- httr::content(res, as = "parsed")
-    stop(sprintf("Failed to retrieve message with id %s. Error: %s (code: %s)", 
-                 messageId, res, statCode))
+  # Process the result.
+  {
+    if (res$status_code != 200) {
+      statCode <- res$status_code
+      res <- httr::content(res, as = "parsed")
+      stop(sprintf("Failed to retrieve message with id %s. Error: %s (code: %s)", 
+                   messageId, res, statCode))
+    }
+    
+    res <- httr::content(x = res, as = "parsed")
+    
+    if (decode) {
+      res <- jsonlite::fromJSON(
+        rawToChar(openssl::base64_decode(text = res$message)))
+    }
   }
   
-  res <- httr::content(x = res, as = "parsed")
-  
-  if (decode) {
-    res <- jsonlite::fromJSON(
-      rawToChar(openssl::base64_decode(text = res$message)))
-  }
-  
+  # Add the message ID to the result - just for user convenience.
   res$message_id <- messageId
   
+  # Done.
   return(res)
   
 }
